dc5cc6cc1e4c18894eb45b64f149c596d083249b
“JyothiThipplikat” <lilmuthu4life21@gmail.com>
2018-09-05 21:31:48 -0500

    sort and sort order

---
+++ app/controllers/api/recipes_controller.rb
@@ -0,0 +1,65 @@
+  class Api::RecipesController < ApplicationController
+  def index
+    @recipes = Recipe.all
+
+    search_term = params[:search]
+    if search_term
+      @recipes = Recipe.where(
+                              "title iLIKE ? OR directions iLIKE ? OR ingredients iLIKE ?", 
+                              "%#{search_term}%",
+                              "%#{search_term}%",
+                              "%#{search_term}%"
+                             )
+    end
+
+    sort_attribute = params[:sort]
+    sort_order = params[:sort_order]
+    
+    if sort_attribute && sort_order
+      @recipes =@recipes.order(sort_attribute => sort_order)
+    elsif sort_order
+      @recipes = @recipes.order(sort_attribute)
+    end
+
+    render 'index.json.jbuilder'
+  end
+
+  def create
+    @recipe = Recipe.new(
+                         title: params[:title],
+                         chef: params[:chef],
+                         ingredients: params[:ingredients],
+                         directions: params[:directions],
+                         image_url: params[:image_url],
+                         prep_time: params[:prep_time]
+                        )
+    @recipe.save
+    render 'show.json.jbuilder'
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    render 'show.json.jbuilder'
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+
+    @recipe.title = params[:title] || @recipe.title
+    @recipe.chef = params[:chef] || @recipe.chef
+    @recipe.prep_time = params[:prep_time] || @recipe.prep_time
+    @recipe.ingredients = params[:ingredients] || @recipe.ingredients
+    @recipe.directions = params[:directions] || @recipe.directions
+    @recipe.image_url = params[:image_url] || @recipe.image_url
+
+    @recipe.save
+    render "show.json.jbuilder"
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    render json: {message: "Recipe successfully destroyed"}
+  end
+end
+
---
+++ app/views/api/recipes/_recipes.json.jbuilder
@@ -0,0 +1,16 @@
+json.id recipe.id
+json.title recipe.title
+json.chef recipe.chef
+json.prep_time recipe.prep_time
+json.ingredients recipe.ingredients
+json.directions recipe.directions
+json.image_url recipe.image_url
+
+
+json.formatted do
+  json.created_at recipe.created_at
+  json.prep_time recipe.prep_time
+  json.ingredients recipe.ingredients_list
+  json.directions recipe.directions_list
+
+  end
